72327b05df039f655df30613347be318f32ce3d0
bbbirkan <58374229+bbbirkan@users.noreply.github.com>
2021-01-24 16:09:24 -0500

    Add files via upload

---
+++ microsoft_translate.py
@@ -0,0 +1,55 @@
+import requests, uuid, json,time
+try:
+    with open("azure_english.json", "r") as jsonFile:
+        data = json.load(jsonFile)   
+except:
+    pass 
+
+
+subscription_key = "take api key number and paste here" #web page =https://docs.microsoft.com/en-us/azure/cognitive-services/translator/?WT.mc_id=python-10851-chrhar
+endpoint = "https://api.cognitive.microsofttranslator.com"
+
+location = "write api location here"
+
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    'from': 'en',
+    'to': ['tr']
+}
+constructed_url = endpoint + path
+
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+# You can pass more than one object in body.
+f = open("/Users/birkankalyon/Desktop/google-10000-english-master/20k.txt", "r")
+for line in f:
+    time.sleep(1)
+    line=str(line)
+    line=line.replace("\n","")
+    body = {
+        'text':line
+    }
+    key = (body['text'])
+    body = [body]
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+
+    response = request.json()
+
+    response = str(response)
+    response = response[29:-17]
+    data[key] = response
+
+    with open("azure_english.json", "w") as ok:
+        json.dump(data, ok, ensure_ascii=False, indent=1)
+
+
+
+
